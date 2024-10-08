b08f213a3650bbbe2f81ae648eef1e3de2a8f556
Mylonas <mylonasg88@gmail.com>
2024-10-08 19:38:53 +0300

    Created second form to test the difference of validation.

--- app/ui/invoices/invoice-form.tsx
+++ app/ui/invoices/invoice-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useActionState, useEffect, useState } from 'react';
+import { CurrencyDollarIcon } from '@heroicons/react/24/outline';
 
 import { Button } from '../components/button';
 
@@ -18,6 +19,7 @@ export default function InvoiceForm({ customers }: InvoiceFormProps) {
 
   useEffect(() => {
     console.log('You had some errors.', errors);
+    console.log(errors.errors);
     if (errors?.success) setSuccess(true);
     else setSuccess(false);
   }, [errors]);
@@ -28,41 +30,76 @@ export default function InvoiceForm({ customers }: InvoiceFormProps) {
 
   return (
     <form action={formAction}>
-      {isPending && <h4 className="text-blue-500">Saving...</h4>}
-      {success ? (
-        <h4 className="text-md text-green-700 p-2 w-fit rounded-xl bg-slate-300 block">
-          Invoice was saved successfully
-        </h4>
-      ): <h4 className="text-md font-thin text-red-700 p-2 w-fit rounded-xl bg-slate-300 block">
-      Something didn't work..
-    </h4> } 
-      <div className="mt-4">
-        <label htmlFor="customerId">User ID:</label>
-        <select id="customerId" name="customerId" required>
-          <option value="">Select a user</option>
-          {customers?.map((customer) => (
-            <option key={customer.id} value={customer.id}>
-              {customer.name}
-            </option>
-          ))}
-        </select>
-      </div>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {isPending && <h4 className="text-blue-500">Saving...</h4>}
+        {success ? (
+          <h4 className="text-md text-green-700 p-2 w-fit rounded-xl bg-slate-300 block">
+            Invoice was saved successfully
+          </h4>
+        ) : (
+          <h4 className="text-md font-thin text-red-700 p-2 w-fit rounded-xl bg-slate-300 block">
+            Something didn't work..
+          </h4>
+        )}
+        <div className="mb-4">
+          <label htmlFor="customerId">User ID:</label>
+          <div className="relative">
+            <select id="customerId" name="customerId">
+              <option value="">Select a user</option>
+              {customers?.map((customer) => (
+                <option key={customer.id} value={customer.id}>
+                  {customer.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <ErrorMessenger errors={errors} fieldName="customerId" />
+        </div>
 
-      <div>
-        <label htmlFor="amount">Amount:</label>
-        <input type="number" id="amount" name="amount" min={1} />
-      </div>
+        <div className="mb-4">
+          <label className="mb-2 block text-sm font-medium" htmlFor="amount">
+            Amount:
+          </label>
+          <div className="relative rounded-md">
+            <input
+              placeholder="Enter USD amount"
+              className="peer block w-full rounded-md border border-gray-200 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              type="number"
+              id="amount"
+              name="amount"
+              min={1}
+            />
+            <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
+          <ErrorMessenger errors={errors} fieldName="amount" />
+          {/* {errors?.errors?.amount && <p className="text-red-500 text-sm">{errors?.errors?.amount[0]}</p>} */}
+        </div>
 
-      <div>
-        <label htmlFor="status">Status:</label>
-        <select id="status" name="status" required>
-          <option value="">Select status</option>
-          <option value="pending">Pending</option>
-          <option value="paid">Paid</option>
-        </select>
-      </div>
+        <div className="mb-4">
+          <label htmlFor="status">Status:</label>
+          <select id="status" name="status">
+            <option value="">Select status</option>
+            <option value="pending">Pending</option>
+            <option value="paid">Paid</option>
+          </select>
+          <ErrorMessenger errors={errors} fieldName="status" />
+          {/* {errors?.errors?.status && <p className="text-red-500 text-sm">{errors?.errors?.status[0]}</p>} */}
+        </div>
 
-      <Button>Create Invoice</Button>
+        <Button>Create Invoice</Button>
+      </div>
     </form>
   );
 }
+
+type ErrorMessengerType = {
+  errors: { [key: string]: Array<string> };
+  fieldName: string;
+};
+
+const ErrorMessenger = ({errors, fieldName}: ErrorMessengerType ) => {
+  if (!errors) return null;
+  if (!errors?.errors?.[fieldName]) return null;
+
+  return <p className="text-red-500 text-sm mt-2">{errors?.errors[fieldName][0]}</p>;
+};
